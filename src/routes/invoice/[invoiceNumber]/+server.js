2635c291e4177022a236f19bb623ad415a2b66fb
Matt Bruer <mbruer@gmail.com>
2024-04-05 22:50:29 -0500

    to number

--- src/routes/invoice/[invoiceNumber]/+server.js
+++ src/routes/invoice/[invoiceNumber]/+server.js
@@ -3,6 +3,7 @@ import { invoices } from '../../../db/invoices';
 
 export async function POST({ request }) {
 	const data = await request.json();
+	data.invoiceNumber = +data.invoiceNumber;
 	const dbres = await invoices.insertOne(data);
 	const insertedInvoice = await invoices.findOne({ _id: dbres.insertedId });
 
@@ -15,8 +16,8 @@ export async function PUT({ request }) {
 	const data = await request.json();
 
 	const updated = await invoices.findOneAndReplace(
-		{ invoiceNumber: data.invoiceNumber },
-		{ ...data }
+		{ invoiceNumber: +data.invoiceNumber },
+		{ ...data, invoiceNumber: +data.invoiceNumber }
 	);
 
 	return json(updated);
